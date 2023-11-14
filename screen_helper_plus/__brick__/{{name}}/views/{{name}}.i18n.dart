426545598f364c9b60a9a8dd08123d98d64b46e2
Sangam-at-712176428654 <66767187+codersangam@users.noreply.github.com>
2023-11-14 11:17:48 +0400

    fixed translations

--- screen_helper_plus/__brick__/{{name}}/views/{{name}}.i18n.dart
+++ screen_helper_plus/__brick__/{{name}}/views/{{name}}.i18n.dart
@@ -1,17 +1,21 @@
 part of '{{name}}_imports.dart';
 
+const hello = "Hello";
+const world = "World";
+
 extension Localization on String {
-  static final _t = Translations("en_us") +
-      {
-        "en_us": "Hello",
-        "ne_np": "नमस्ते",
-        "hi_in": "नमस्ते",
-      } +
-      {
-        "en_us": "World",
-        "ne_np": "संसार",
-        "hi_in": "दुनिया",
-      };
+  static const _t = Translations.from("en_us", {
+    hello: {
+      "en_us": "Hello",
+      "ne_np": "नमस्ते",
+      "hi_in": "नमस्ते",
+    },
+    world: {
+      "en_us": "World",
+      "ne_np": "संसार",
+      "hi_in": "दुनिया",
+    },
+  });
 
   String get i18n => localize(this, _t);
   String fill(List<Object> params) => localizeFill(this, params);
